7abcac98661090ca5d6818370b79c31062370075
sg.sharov <sg.sharov@tensor.ru>
2022-05-28 00:20:55 +0300

    feat: сделал переключатель типа и проброс значения в app

--- src/components/TypeSwitcher/TypeSwitcher.js
+++ src/components/TypeSwitcher/TypeSwitcher.js
@@ -1,5 +1,30 @@
-const TypeSwitcher = () => {
-	return <div>TypeSwitcher</div>;
+import './TypeSwitcher.css';
+
+const TypeSwitcher = ({status, changeStatus}) => {
+	const handleChangeStatus = () => {
+		changeStatus(status);
+	};
+
+	return (
+		<div className='type-switcher'>
+			<button
+				className={`type-switcher__button ${
+					status ? 'type-switcher__button_active' : ''
+				}`}
+				onClick={handleChangeStatus}
+			>
+				CURRENT
+			</button>
+			<button
+				className={`type-switcher__button ${
+					status ? '' : 'type-switcher__button_active'
+				}`}
+				onClick={handleChangeStatus}
+			>
+				7 DAY FORECAST
+			</button>
+		</div>
+	);
 };
 
 export default TypeSwitcher;
