a0a0c5d76cbc2b495f80f4538b578a0626d49c37
sg.sharov <sg.sharov@tensor.ru>
2022-05-28 17:15:43 +0300

    feat: реализовал блок с названием города и страны, получение названия с источника

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,36 +10,88 @@ import City from '../City/City';
 import Weather from '../Weather/Weather';
 
 function App() {
-	const [currentCity, setCurrentCity] = useState('');
-
+	const [currentCity, setCurrentCity] = useState('London');
 	const [isCurrent, setIsCurrent] = useState(true);
+
+	const [currentWeather, setCurrentWeather] = useState({});
+	const [weatherForWeek, setWeatherForWeek] = useState({});
 
 	useEffect(() => {
 		Api.getCurrentWeather('London').then((weather) => {
 			console.log(weather);
+			setCurrentWeather(weather);
 		});
 		Api.getWeatherForWeek('London').then((weekWaether) => {
 			console.log(weekWaether);
+			setWeatherForWeek(weekWaether);
 		});
 	}, []);
 
 	const searchCity = (query) => {
 		setCurrentCity(query);
+		if (isCurrent) {
+			Api.getCurrentWeather(query).then((weather) => {
+				console.log(weather);
+				setCurrentWeather(weather);
+			});
+		} else {
+			Api.getWeatherForWeek(query).then((weekWaether) => {
+				console.log(weekWaether);
+				setWeatherForWeek(weekWaether);
+			});
+		}
 	};
 
 	const changeStatus = (status) => {
 		setIsCurrent(!status);
 		console.log(status);
+		if (status) {
+			Api.getWeatherForWeek(currentCity).then((weekWaether) => {
+				console.log(weekWaether);
+				setWeatherForWeek(weekWaether);
+			});
+		} else {
+			Api.getCurrentWeather(currentCity).then((weather) => {
+				console.log(weather);
+				setCurrentWeather(weather);
+			});
+		}
 	};
 
 	return (
 		<div className='App'>
 			<Search searchCity={searchCity} />
 			<TypeSwitcher status={isCurrent} changeStatus={changeStatus} />
-			<City currentCityValue={currentCity} />
+			<City
+				currentCityValue={
+					isCurrent ? currentWeather.name : weatherForWeek.city.name
+				}
+				country={
+					isCurrent
+						? currentWeather.sys
+							? currentWeather.sys.country
+							: ''
+						: weatherForWeek.city.country
+				}
+			/>
 			<Weather isCurrent={isCurrent} />
 		</div>
 	);
 }
 
 export default App;
+
+/*
+
+currentCityValue={
+					isCurrent ? currentWeather.name : weatherForWeek.city.name
+				}
+
+
+country={
+					isCurrent ? currentWeather.sys.country : weatherForWeek.city.country
+				}
+
+
+				country={isCurrent ? currentWeather.sys : weatherForWeek.city}
+				*/
